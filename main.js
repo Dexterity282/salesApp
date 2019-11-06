02ba3536d9f60459bea4b2fdb57559c3ae33da9d
Peter <dexterity2soar@gmail.com>
2019-11-06 11:39:50 +0100

    This is our first SalesApp commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function pay() {
+var MOP = document.getElementById("sell").value;
+var Amt = document.getElementById("num").value;
+var Cat = document.getElementById("sel2").value;
+var Des = document.getElementById("comment").value;
+document.getElementById("MOP").innerHTML = MOP;
+document.getElementById("Amt").innerHTML = Amt;
+document.getElementById("Cat").innerHTML = Cat;
+document.getElementById("Des").innerHTML = Des;
+}
