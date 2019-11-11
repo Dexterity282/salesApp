e78dd4c8a3204c4dabee451934444078e4875e02
Peter <dexterity2soar@gmail.com>
2019-11-11 15:01:37 +0100

    first commit

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ document.getElementById("Amt").innerHTML = Amt;
 document.getElementById("Cat").innerHTML = Cat;
 document.getElementById("Des").innerHTML = Des;
 }
+
+/*var pay = [];
+pay.push({Number:numm,category:copy,amount:amt,MOP:mop});
+
+/*for(i=0; i < pay.length; i++ ){
+num +=pay[i] + "<br>";
+
+}
+for(i=0; i < pay.length; i++){
+num +="<tr><td>" + Number(i+1) + "</td><td>"+pay[i].category+"</td><td>"+pay[i].amount+"</td><td>"+pay[i].MOP+"</td></tr>";
+}
+document.getElementById("demo").innerHTML = num;*/
